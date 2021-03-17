f19ec39b8f7c23fdfb7e88e8a275b0ce3fa4fabf
abraham <abraham@newverveconsulting.com>
2021-03-17 21:05:24 +0000

    Deleting some spaces

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React, { useState, useEffect } from "react";
 import Dice from "./components/Dice";
 
 export default function App() {
-
-
   return (
     <div className="App">
       {/* <ColumnnContainer> */}
       <h1>D&D Dice</h1>
-      
       <Dice key="one"/>
       <Dice key="two"/>
     </div>
