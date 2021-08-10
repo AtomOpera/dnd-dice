5ada6fea53083226f0946dc9e929a79e27098e8c
abraham <abraham@newverveconsulting.com>
2021-08-10 09:39:31 +0100

    more dice to roll now up to 20

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -130,6 +130,11 @@ export default function Dice() {
               <option value="13">13</option>
               <option value="14">14</option>
               <option value="15">15</option>
+              <option value="16">16</option>
+              <option value="17">17</option>
+              <option value="18">18</option>
+              <option value="19">19</option>
+              <option value="20">20</option>
             </select>
           {/* <input 
             type="text" 
